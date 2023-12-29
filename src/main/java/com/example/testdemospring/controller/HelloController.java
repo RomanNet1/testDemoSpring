ede63a2ea7fef3748f00e9d4f6781c2013818cce
RomanNet1 <ramulrimov004@gmail.com>
2023-12-29 17:15:08 +0300

    Value

--- src/main/java/com/example/testdemospring/controller/HelloController.java
+++ src/main/java/com/example/testdemospring/controller/HelloController.java
@@ -1,19 +1,21 @@
 package com.example.testdemospring.controller;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@ConfigurationProperties("hello")
+//@ConfigurationProperties("hello")
 public class HelloController {
+    @Value("${hello.from:undefined}")
     private  String from;
 
-    public void setFrom(String from) {
-        this.from = from;
-    }
+//    public void setFrom(String from) {
+//        this.from = from;
+//    }
 
     @GetMapping("/")
     public String hello() {
